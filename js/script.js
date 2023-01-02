fa609ad849eb7785f2f76e3aa467e214c24a0a0e
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-01-02 10:43:51 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 4000
+    });
+
+
+
+    if($('.progress-line').length) {
+        $('.progress-line').appear(function(){
+            var el = $(this);
+            var percent = el.data('width');
+            $(el).css('width', percent+'%');
+        }, {accY: 0});
+    }
+
+
+
+});
